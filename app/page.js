1fbc963f5f4b8dde7f927cb47dc32d08da7976f6
Facu Adaro <facundoadaro0@gmail.com>
2024-02-27 15:35:08 -0300

    FAQ mejorado, segundo commit

--- app/page.js
+++ app/page.js
@@ -691,6 +691,138 @@ export default function Home() {
         </div>
       </section>
       {/* -------------------- */}
+
+      {/* Acá comienzan FAQs */}
+      <section className="bg-gray-900 py-16 px-28">
+        <div className="grid justify-center py-4">
+          <details className="group rounded-md max-w-6xl border-s-4 border-green-500 bg-gray-50 p-6 [&_summary::-webkit-details-marker]:hidden">
+            <summary className="flex cursor-pointer items-center justify-between gap-1.5">
+              <h2 className="text-lg px-52 font-medium text-gray-900">
+                Lorem ipsum dolor sit amet consectetur adipisicing?
+              </h2>
+
+              <span className="shrink-0 rounded-full bg-white p-1.5 text-gray-900 sm:p-3">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="size-5 shrink-0 transition duration-300 group-open:-rotate-45"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M10 3a1 1 0 011 1v5h5a1 1 0 110 2h-5v5a1 1 0 11-2 0v-5H4a1 1 0 110-2h5V4a1 1 0 011-1z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+              </span>
+            </summary>
+
+            <p className="mt-4 leading-relaxed text-gray-700">
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ab hic
+              veritatis molestias culpa in, recusandae laboriosam neque aliquid
+              libero nesciunt voluptate dicta quo officiis explicabo
+              consequuntur distinctio corporis earum similique!
+            </p>
+          </details>
+        </div>
+
+        <div className="grid justify-center py-4">
+          <details className="group rounded-md max-w-6xl border-s-4 border-green-500 bg-gray-50 p-6 [&_summary::-webkit-details-marker]:hidden">
+            <summary className="flex cursor-pointer items-center justify-between gap-1.5">
+              <h2 className="text-lg px-52 font-medium text-gray-900">
+                Lorem ipsum dolor sit amet consectetur adipisicing?
+              </h2>
+
+              <span className="shrink-0 rounded-full bg-white p-1.5 text-gray-900 sm:p-3">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="size-5 shrink-0 transition duration-300 group-open:-rotate-45"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M10 3a1 1 0 011 1v5h5a1 1 0 110 2h-5v5a1 1 0 11-2 0v-5H4a1 1 0 110-2h5V4a1 1 0 011-1z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+              </span>
+            </summary>
+
+            <p className="mt-4 leading-relaxed text-gray-700">
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ab hic
+              veritatis molestias culpa in, recusandae laboriosam neque aliquid
+              libero nesciunt voluptate dicta quo officiis explicabo
+              consequuntur distinctio corporis earum similique!
+            </p>
+          </details>
+        </div>
+
+        <div className="grid justify-center py-4">
+          <details className="group rounded-md max-w-6xl border-s-4 border-green-500 bg-gray-50 p-6 [&_summary::-webkit-details-marker]:hidden">
+            <summary className="flex cursor-pointer items-center justify-between gap-1.5">
+              <h2 className="text-lg px-52 font-medium text-gray-900">
+                Lorem ipsum dolor sit amet consectetur adipisicing?
+              </h2>
+
+              <span className="shrink-0 rounded-full bg-white p-1.5 text-gray-900 sm:p-3">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="size-5 shrink-0 transition duration-300 group-open:-rotate-45"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M10 3a1 1 0 011 1v5h5a1 1 0 110 2h-5v5a1 1 0 11-2 0v-5H4a1 1 0 110-2h5V4a1 1 0 011-1z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+              </span>
+            </summary>
+
+            <p className="mt-4 leading-relaxed text-gray-700">
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ab hic
+              veritatis molestias culpa in, recusandae laboriosam neque aliquid
+              libero nesciunt voluptate dicta quo officiis explicabo
+              consequuntur distinctio corporis earum similique!
+            </p>
+          </details>
+        </div>
+
+        <div className="grid justify-center py-4">
+          <details className="group rounded-md max-w-6xl border-s-4 border-green-500 bg-gray-50 p-6 [&_summary::-webkit-details-marker]:hidden">
+            <summary className="flex cursor-pointer items-center justify-between gap-1.5">
+              <h2 className="text-lg px-52 font-medium text-gray-900">
+                Lorem ipsum dolor sit amet consectetur adipisicing?
+              </h2>
+
+              <span className="shrink-0 rounded-full bg-white p-1.5 text-gray-900 sm:p-3">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="size-5 shrink-0 transition duration-300 group-open:-rotate-45"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M10 3a1 1 0 011 1v5h5a1 1 0 110 2h-5v5a1 1 0 11-2 0v-5H4a1 1 0 110-2h5V4a1 1 0 011-1z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+              </span>
+            </summary>
+
+            <p className="mt-4 leading-relaxed text-gray-700">
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ab hic
+              veritatis molestias culpa in, recusandae laboriosam neque aliquid
+              libero nesciunt voluptate dicta quo officiis explicabo
+              consequuntur distinctio corporis earum similique!
+            </p>
+          </details>
+        </div>
+      </section>
+      {/* -------------------- */}
     </>
   );
 }
