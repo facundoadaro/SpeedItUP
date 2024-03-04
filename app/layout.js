d16ec30696cc858a6c4c0f4f1a29560cdcc5a11b
Facu Adaro <facundoadaro0@gmail.com>
2024-03-04 10:30:28 -0300

    cambio de ubicación de speedinsights

--- app/layout.js
+++ app/layout.js
@@ -15,11 +15,11 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <SpeedInsights>
+        <SpeedInsights></SpeedInsights>
           <Header></Header>
           {children}
           <Footer></Footer>
-        </SpeedInsights>
+        
       </body>
     </html>
   );
